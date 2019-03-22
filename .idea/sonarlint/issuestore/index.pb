4ce4fbf85f3f1806c083dd5077fb19fd347e757c
John Adeshola <timadeshola@yahoo.com>
2019-03-22 07:30:57 +0100

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+h
+8src/main/java/Utils/FraudulentActivityNotifications.java,6\c\6cb24d7c0c8d5a52dff2d55da2fba85a0923217d
+Y
+)src/main/java/Utils/ApplicationUtils.java,a\6\a6f3452a2178603c638989f7621a9a9ea61da389
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+Q
+!src/main/java/Utils/Solution.java,a\4\a48af1618b5b6733fb0c92647d071c0b3aa31c96
+[
++src/main/java/Utils/FraudulentActivity.java,d\0\d01ecb68dcf00a99055e56171f08710704a9e865
---
+++ src/main/java/FraudPreventionAndNotification.java
@@ -0,0 +1,97 @@
+import java.util.*;
+
+/**
+ * Created by timad on 3/22/2019.
+ */
+public class FraudPreventionAndNotification {
+
+
+    public static PriorityQueue<Integer> lowerPriorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+    public static PriorityQueue<Integer> higherPriorityQueue = new PriorityQueue<>();
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int day = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+        long notificationCount = 0;
+
+        for(int i=0; i<number; i++) {
+            int x = sc.nextInt();
+            list.add(x);
+            int first = -1;
+
+            if(i < day){
+                first = -1;
+            } else if(i >= day) {
+                first = list.remove(0);
+            }
+
+            double median = calculateMedian(x,first,i,day);
+
+            if(i < day) {
+                continue;
+            } else if(i >= day) {
+                if(x >= 2 *median)
+                    notificationCount++;
+            }
+        }
+
+        System.out.println(notificationCount);
+    }
+
+    public static void addNumber(int number, PriorityQueue<Integer> lower, PriorityQueue<Integer> upper)
+    {
+        if(lower.size() == 0 || number<lower.peek()) {
+            lower.add(number);
+        } else {
+            upper.add(number);
+        }
+    }
+
+
+    public static void calculateBalance(PriorityQueue<Integer> lowers, PriorityQueue<Integer> highers) {
+
+        PriorityQueue<Integer> biggerHeap = lowers.size() > highers.size() ? lowers:highers;
+        PriorityQueue<Integer> smallerHeap = lowers.size() > highers.size() ? highers:lowers;
+
+        if(biggerHeap.size() - smallerHeap.size() >= 2) {
+            smallerHeap.add(biggerHeap.poll());
+        }
+    }
+
+    public static double calculateMedian(PriorityQueue<Integer> lowers, PriorityQueue<Integer> highers) {
+        PriorityQueue<Integer> biggerHeap = lowers.size() > highers.size() ? lowers:highers;
+        PriorityQueue<Integer> smallerHeap = lowers.size() > highers.size() ? highers:lowers;
+
+        if(biggerHeap.size() == smallerHeap.size()) {
+            return  ((double) biggerHeap.peek()+smallerHeap.peek())/2;
+        } else {
+            return biggerHeap.peek();
+        }
+    }
+
+    public static double calculateMedian(int number, int first, int position, int win) {
+        if (position < win) {
+            addNumber(number, lowerPriorityQueue, higherPriorityQueue);
+            calculateBalance(lowerPriorityQueue, higherPriorityQueue);
+            return 0.0;
+        } else if (position >= win) {
+            double median = calculateMedian(lowerPriorityQueue, higherPriorityQueue);
+            if (lowerPriorityQueue.peek() < first) {
+                higherPriorityQueue.remove(new Integer(first));
+            } else {
+                lowerPriorityQueue.remove(new Integer(first));
+            }
+
+            addNumber(number, lowerPriorityQueue, higherPriorityQueue);
+            calculateBalance(lowerPriorityQueue, higherPriorityQueue);
+            return median;
+        }
+
+
+        return 0.0;
+    }
+}
